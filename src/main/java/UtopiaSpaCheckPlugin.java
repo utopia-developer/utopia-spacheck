12f70cf3d85796dd13ca428c516eca2fe59c2750
utopia-developer <utopia0developer@gmail.com>
2021-09-01 22:54:44 +0800

    [feat][apply] 插件入口类^未完成

---
+++ src/main/java/UtopiaSpaCheckPlugin.java
@@ -0,0 +1,17 @@
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * @author Utopia
+ */
+public class UtopiaSpaCheckPlugin implements Plugin<Project> {
+
+    private static final String TASK_NAME = "utopia_check";
+
+    private UtopiaSpaCheckExtension utopiaSpaCheckExtension;
+
+    @Override
+    public void apply(Project target) {
+
+    }
+}
